94ab3333fda86813f3b36a5504570e72cdb4e11c
Antho <ghostof83@hotmail.fr>
2017-04-25 14:21:04 +0200

    inscr

--- site/site/main.py
+++ site/site/main.py
@@ -3,6 +3,7 @@
 
 from flask import *
 import psycopg2
+from random import randrange
 app = Flask(__name__)
 
 
@@ -12,9 +13,8 @@ def connect():
         print("\nConnecté.\n")
     except:
         print("\n##### ERREUR DE CO #####\n")
-    cur = conn.cursor()
-    cur.execute("SET search_path TO chtulhu")
-    return cur
+    return conn
+
 
 @app.route('/')
 def accueil():
@@ -22,32 +22,67 @@ def accueil():
     l_css = ["acceuil.css"]
     return render_template('layout_base.html', section=section, l_css=l_css)
 
+
 @app.route('/articles/')
 def articles():
     section = "articles.html"
     l_css = ["articles.css"]
-    articles = [1,2,3]
+    articles = [1, 2, 3]
     return render_template('layout_base.html', section=section,
-                                               l_css=l_css
-                                               ,articles=articles)
+                           l_css=l_css, articles=articles)
+
 
 @app.errorhandler(404)
 def err(error):
     return ("ERROR {} CHTULHU NOT FOUND (lol)".format(error.code), error.code)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        query = "INSERT INTO Client (numClient, nomClient) VALUES (20, 'Marc');"
-        cur.execute(query)
-        query = "SELECT * FROM Client;"
-        cur.execute(query)
-        cur.fetchall()
-        return redirect(url_for('accueil'))
+        cur.execute("SELECT mailClient FROM Client;")
+        l_client = cur.fetchall()
+        print l_client
+        mailC = request.form["mail"]
+        if (len(l_client) != 0):
+            i = 0
+            while (i < len(l_client)):
+                if (l_client[i][0] == mailC):
+                    return redirect(url_for('articles'))
+                i += 1
+        return render_template('acceuil.html')
     else:
-        return render_template('login.html')
+        section = "login.html"
+        l_css = ["acceuil.html"]
+        return render_template('layout_base.html', section=section, l_css=l_css)
+
+
+@app.route('/subscription/', methods=['POST'])
+def subscription():
+    cur.execute("SELECT * FROM Client;")
+    l_client = cur.fetchall()
+    print l_client
+    numC = randrange(20, 30)
+    nom = request.form["nom"]
+    prenom = request.form["prenom"]
+    mailC = request.form["mail"]
+    if (len(l_client) != 0):
+        i = 0
+        while (i < len(l_client)):
+            if (l_client[i][4] == mailC):
+                return render_template('acceuil.html')
+            if (l_client[i][0] == numC):
+                numC = randrange(20, 30)
+                i = -1
+            i += 1
+    query = "INSERT INTO Client (numClient, nomClient, prenomClient, mailClient) \
+             VALUES ('"+str(numC)+"', '"+nom+"', '"+prenom+"', '"+mailC+"');"
+    cur.execute(query)
+    conn.commit()
+    return redirect(url_for('articles'))
 
 if __name__ == '__main__':
-    #cur = connect()
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("SET search_path TO chtulhu")
     app.run(debug=True)
